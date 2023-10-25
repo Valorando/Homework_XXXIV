65d878f309c51d76e44568d66e2aafdf02a82cdb
Valorando <vadimsokil.dnipro@gmail.com>
2023-10-25 17:51:25 +0300

    Almost done.

---
+++ TableContext/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Text;
+using TableModel;
+
+
+namespace TableContext
+{
+    public class StandingsContext : DbContext
+    {
+        public StandingsContext()
+        :base("DBConnection")
+        { }
+
+        public DbSet<Standings> Standings { get; set; }
+    }
+}
---
+++ TableModel/Class1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Interfaces.Streaming;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace TableModel
+{
+    public class Standings
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Place { get; set; }
+        public int Win { get; set; }
+        public int Loss { get; set; }
+        public int Draw { get; set; }
+    }
+}
